3d4ff99705313fe5fb8525471fc0e0ae7173e85b
Gennadya <heavyyrain3039@gmail.com>
2019-05-10 15:56:32 +0300

    создание директривы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
   ],
   imports: [
     BrowserModule,
