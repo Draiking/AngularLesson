862b447320f71e2af88b960d12f5ad21d95c689c
Gennadya <heavyyrain3039@gmail.com>
2019-05-22 16:59:39 +0300

    render

--- src/app/directives/background.directiv.ts
+++ src/app/directives/background.directiv.ts
@@ -10,6 +10,7 @@ export class BackgroundDirective implements OnInit {
 
     ngOnInit() {
         this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+        this.renderer.addClass(this.element.nativeElement, 'white-text');
         /*this.element.nativeElement.style.backgroundColor = 'red';*/
     }
 }
