68b27c3cdb101b6227f9ead1e8361019274fd07c
Gennadya <heavyyrain3039@gmail.com>
2019-05-22 11:11:25 +0300

    создание стиля через рендер

--- src/app/directives/background.directiv.ts
+++ src/app/directives/background.directiv.ts
@@ -1,14 +1,15 @@
-import {ElementRef, Directive, OnInit} from '@angular/core';
+import {ElementRef, Directive, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appBackground]'
 })
 
 export class BackgroundDirective implements OnInit {
-    constructor(private element: ElementRef) {
+    constructor(private element: ElementRef, private renderer: Renderer2) {
     }
 
     ngOnInit() {
-        this.element.nativeElement.style.backgroundColor = 'red';
+        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+        /*this.element.nativeElement.style.backgroundColor = 'red';*/
     }
 }
