48a95f9ef4ff07d989422e5e4959c0540d24a945
Gennadya <heavyyrain3039@gmail.com>
2019-05-23 16:43:38 +0300

    форма.Вводные

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  headerText = 'Создание своей директривы';
+    answers = [{
+        type: 'yes',
+        text: 'да'
+    }, {
+        type: 'no',
+        text: 'нет'
+    }];
 }
--- src/app/directives/background.directiv.ts
+++ src/app/directives/background.directiv.ts
@@ -17,16 +17,14 @@ export class BackgroundDirective implements OnInit {
     }
 
     @HostListener('mouseenter') mouseEnter() {
-   /*     const {nativeElement} = this.element;
+        const {nativeElement} = this.element;
         this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-        this.renderer.addClass(nativeElement, 'white-text');*/
-        this.background = 'red';
+        this.renderer.addClass(nativeElement, 'white-text');
     }
 
     @HostListener('mouseleave') mouseLeave() {
-       /* const {nativeElement} = this.element;
+        const {nativeElement} = this.element;
         this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
-        this.renderer.removeClass(nativeElement, 'white-text');*/
-        this.background = 'transparent';
+        this.renderer.removeClass(nativeElement, 'white-text');
     }
 }
